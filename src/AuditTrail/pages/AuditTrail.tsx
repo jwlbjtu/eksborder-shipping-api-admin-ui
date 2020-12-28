fefe22e599372615b87d4a2ab27613f3b18a1096
Wenlong Jiang <jwlbjtu@gmail.com>
2020-12-27 21:54:05 -0500

    Client Shipping Panel

--- src/AuditTrail/pages/AuditTrail.tsx
+++ src/AuditTrail/pages/AuditTrail.tsx
@@ -1,7 +1,48 @@
-import React, { ReactElement } from 'react';
+import _ from 'lodash';
+import React, { ReactElement, useEffect, useState } from 'react';
+import { PageHeader, Table, Divider } from 'antd';
 
 const AuditTrail = (): ReactElement => {
-  return <div>Audit Trail</div>;
+  const [tableLoading, setTableLoading] = useState(false);
+
+  useEffect(() => {
+    // TODO: Call back end to get data
+    setTableLoading(true);
+    setTimeout(() => {
+      setTableLoading(false);
+    }, 1000);
+  }, []);
+
+  const columns = [
+    {
+      title: '日期',
+      dataIndex: 'date',
+      key: 'date'
+    },
+    {
+      title: '管理员姓名',
+      dataIndex: 'name',
+      key: 'name'
+    },
+    {
+      title: '用户名称',
+      dataIndex: 'client',
+      key: 'client'
+    },
+    {
+      title: '操作',
+      dataIndex: 'action',
+      key: 'action'
+    }
+  ];
+
+  return (
+    <div>
+      <PageHeader title="操作记录" subTitle="记录所有管理的操作" />
+      <Divider />
+      <Table columns={columns} dataSource={[]} loading={tableLoading} />
+    </div>
+  );
 };
 
 export default AuditTrail;
